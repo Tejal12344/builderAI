944a27a7318a06b49d2b17bf15985d25d0b69864
Tejal <you@example.com>
2026-08-19 11:41:23 +0530

    created user form

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,14 @@
 
 import {createContext,useContext,useState,useEffect}from "react";
 import api from "../api/api";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext(undefined);
 
 export function AppContextProvider({children}){
+ 
+const navigate=useNavigate()
 
 //Auth states
 const[user,setUser]=useState(null)
@@ -14,7 +18,7 @@ const[loadingUser,setLoadingUser]=useState(true);
   const checkSession= async ()=>{
     try{
         const {data}=await api.get("/api/auth/me");
-       // setUser(data.user);
+        setUser(data.user);
     }catch (error){
           setUser(null)
     }finally{
@@ -25,11 +29,46 @@ const[loadingUser,setLoadingUser]=useState(true);
   useEffect(()=>{
     checkSession()
   },[checkSession])
+
+  const login=async (email,password)=>{
+    try{
+        const{data}=await api.post("/api/auth/login",{email,password});
+        setUser(data.user)
+        toast.success("Welcome back!")
+        navigate("/")
+    } catch(err){
+        console.error("Login failed:",err);
+        const errMsg=err?.response?.data?.error || "Invaild email or password";
+        toast.error(errMsg);
+        throw new Error(errMsg);
+        
+
+    }
+  }
+
+  const register=async (name,email,password)=>{
+    try{
+        const{data}=await api.post("/api/auth/register",{name,email,password});
+        setUser(data.user)
+        toast.success("Account created successfully!")
+        navigate("/")
+    } catch(err){
+        console.error("Registration failed:",err);
+        const errMsg=err?.response?.data?.error || "Registration failed";
+        toast.error(errMsg);
+        throw new Error(errMsg);
+        
+
+    }
+  }
   
     return (
         <AppContext.Provider value={{
             user,
-            loadingUser
+            loadingUser,
+            login,
+            register
+
         }}>
             {children}
         </AppContext.Provider>
