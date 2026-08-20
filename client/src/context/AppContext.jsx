8716789974477fcc89a85fdf7af1607555cc6063
Tejal <you@example.com>
2026-08-20 23:31:25 +0530

    created home page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 
-import {createContext,useContext,useState,useEffect}from "react";
+import {createContext,useContext,useState,useEffect, useCallback}from "react";
 import api from "../api/api";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -13,6 +13,24 @@ const navigate=useNavigate()
 //Auth states
 const[user,setUser]=useState(null)
 const[loadingUser,setLoadingUser]=useState(true);
+
+
+//states
+
+const[projects,setProjects]=useState([]);
+const[loadingProjects,setLoadingProjects]=useState(true);
+const[activeProject,setActiveProject]=useState(null);
+const[loadingActiveProject,setLoadingActiveProject]=useState(true);
+const[chatLoading,setChatLoading]=useState(false);
+const[generatingProject,setGeneratingProject]=useState(false);
+const[activeFile,setActiveFile]=useState("/App.js");
+
+const[showCode,setShowCode]=useState(false);
+
+
+
+
+
 
 //Auth Actions
   const checkSession= async ()=>{
@@ -61,13 +79,142 @@ const[loadingUser,setLoadingUser]=useState(true);
 
     }
   }
+
+  const logout=async () => {
+    try{
+        await api.post("/api/auth/logout")
+        setUser(null)
+        setProjects([])
+        setActiveProject(null)
+        toast.success("Logout successfully")
+        navigate("/login")
+    }catch(err){
+        console.error("Logout failed:",err);
+        toast.error("Logout failed");
+
+    }
+    
+  }
+  // projects actions
+  const loadProjects=async () => {
+    if(!user)return;
+    try{
+        const {data}=await api.get("/api/projects")
+        setProjects(data)
+    }catch(err){
+        console.error("Failed to list projects:",err);
+        toast.error("Failed to load projects list");
+    }finally{
+        setLoadingProjects(false);
+    }
+    
+  }
+
+  const loadProject=async(id,silent=false)=>{
+    if(!user) return;
+    if(!silent) setLoadingActiveProject(true)
+        try{
+    const{data}=await api.get(`/api/projects/${id}`)
+    setActiveProject(data);
+
+    //default file selection
+    const files=Object.keys(data.files);
+    if(files.length>0){
+        setActiveFile((prev)=>{
+            if(files.includes(prev)) return "/App.js";
+            return files[0]
+        })
+    }
+    } catch(err){
+        console.error("Failed to load project:",err);
+        if(!silent){
+            toast.error("Failed to load project details");
+            navigate("/");
+        }
+    }finally{
+        if(!silent) setLoadingActiveProject(false)
+    }
+  }
+
+  //automatically poll active project status if generating or pending
+  useEffect(()=>{
+    if(!activeProject?._id || !user)return;
+
+    const isOngoing=activeProject.status==="generating" || activeProject.status===
+    "pending" || activeProject.status==="revising";
+
+    if(isOngoing){
+        setChatLoading(true);
+        const interval=setInterval(()=>{
+            loadProject(activeProject._id,true)},2000);
+            return ()=> clearInterval(interval)
+        }else{
+            setChatLoading(false);
+        }
+
+        },[activeProject?._id,activeProject?.status,loadProject,user])
+
+        const handleGenerate=useCallback(
+            async(prompt)=>{
+                if(!user) return;
+
+                setGeneratingProject(true);
+                try{
+                    const{data}=await api.post("/api/projects",{prompt});
+                    toast.success("AI Agent is planning structure...")
+                    navigate(`/builder/${data._id}`);
+
+                } catch(err){
+                    console.error("Failed to generate project:",err);
+                    toast.error(err?.response?.data?.error || "Failed to generate project");
+
+                }finally{
+                    setGeneratingProject(false);
+                }
+            },[navigate,user]
+        )
+
+        const handleDelete=useCallback(
+            async(id)=>{
+                if(!user)return;
+
+                try{
+                    await api.delete(`/api/projects/${id}`);
+                    setProjects((prev)=>prev.filter((p)=>p._id !==id))
+                    toast.success("Project deleted successfully")
+
+                }catch(err){
+                    console.error("Failed to delete project:",err);
+                    toast.error("Failed to delete project");
+                }
+            },[user]
+
+        )
+    
+  
   
     return (
         <AppContext.Provider value={{
             user,
             loadingUser,
             login,
-            register
+            register,
+            projects,
+            loadingProjects,
+            activeProject,
+            loadingActiveProject,
+            chatLoading,
+            generatingProject,
+            activeFile,
+            showCode,
+            setActiveFile,
+            setShowCode,
+            loadProjects,
+            loadProject,
+            handleGenerate,
+            handleDelete
+
+
 
         }}>
             {children}
